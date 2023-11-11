5202393438b887ebbb7e3d54bdeda0cf951559e9
NathanRosenberg <nmr2290@gmail.com>
2023-11-10 18:12:14 -0600

    Almost done with app, everything coming nicely.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sqlalchemy-challenge
 Starting homework and making decent progress. Getting to the last 12 months of precipitation seems like I will need to import some form of timedelta to get accurate years. Finished what I believe is about half of the assignment. The python file is written and now I need to create the api.
+
+The app is coming along nicely. Mixing code from the last class of advanced SQL and chatgpt I can come up with the correct syntax without too much issues.
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,93 @@
 # Import the dependencies.
-
-
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
+station_counts = session.query(Measurement.station, func.count(Measurement.station)).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).all()
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    all_precip = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict[date] = prcp
+        all_precip.append(precipitation_dict)
+
+    return jsonify(all_precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    stations_list = session.query(Station.station).all()
+    stations = [station[0] for station in stations_list]
+    return jsonify(stations)
+
+@app("/api/v1.0/tobs")
+def tobs():
+    most_active_station = station_counts[0]
+    most_active_station_id = most_active_station[0]
+
+    most_recent_date = session.query(func.max(Measurement.date)).filter(Measurement.station == most_active_station_id).scalar()
+    most_recent_date = datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_ago = most_recent_date - timedelta(days=365)
+
+    last_year_data = session.query(Measurement.date, Measurement.tobs).filter(
+        Measurement.station == most_active_station_id
+        Measurement.date >= one_year_ago.strftime('%Y-%m-%d')
+    ).all()
+
+    temperature_data = {date: tobs for date, tobs in last_year_data}
+
+    return jsonify(temperature_data)
+
+@app.route("/api/v1.0/<start>")
